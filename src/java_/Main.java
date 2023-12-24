3faacc7f5c6cd89456232328eb9a6a88a4aff139
kandeel <youssifk000@gmail.com>
2023-12-24 09:20:12 +0200

    editing the userprofile

--- src/java_/Main.java
+++ src/java_/Main.java
@@ -1,11 +1,8 @@
 package java_;
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.image.* ;
 import javafx.stage.Stage;
 
 public class Main extends Application {
